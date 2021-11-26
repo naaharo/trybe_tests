f07d1d74baaecca1bf54d8b4770a4bc9cc54d89d
Natã A.Rodrigues <nata26ar@gmail.com>
2021-11-26 19:08:53 -0300

    Exercicio 4 feito

--- Fundamentos/Bloco4/Dia1/scripts.js
+++ Fundamentos/Bloco4/Dia1/scripts.js
@@ -1,5 +1,5 @@
 /* Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. */
-console.log ("Exercício 1\n")
+console.log ("Exercício 1\n");
 
 const a = 5;
 const b = 3;
@@ -17,7 +17,7 @@ console.log (div);
 console.log (mod);
 
 /* Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados. */
-console.log ("\n \nExercício 2\n")
+console.log ("\n \nExercício 2\n");
 
 const num12 = 5;
 const num22 = 3;
@@ -31,7 +31,7 @@ if (num12>num22){
 }
 
 /* Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados. */
-console.log ("\n \nExercício 3\n")
+console.log ("\n \nExercício 3\n");
 
 const num13 = 5;
 const num23 = 3;
@@ -54,4 +54,17 @@ if (num13>num23 && num13>num33){
 }
 
 /* Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário. */
-console.log ("\n \nExercício 4\n")
+console.log ("\n \nExercício 4\n");
+
+const num14 = -5;
+
+if (num14 > 0){
+  console.log ("positive");
+}else if (num14 <0){
+  console.log ("negative")
+}else{
+  console.log ("zero")
+}
+
+/* Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro. */
+console.log ("\n \nExercício 5\n");
