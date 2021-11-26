0af39f7e24e7abaddd2ded0062d620339f444f3f
Natã A.Rodrigues <nata26ar@gmail.com>
2021-11-26 18:29:23 -0300

    Pastas do bloco 4

---
+++ Fundamentos/Bloco4/Dia1/scripts.js
@@ -0,0 +1,18 @@
+/* Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. */
+const a = 5;
+const b = 3;
+
+let add = a + b;
+let sub = a - b;
+let mult = a * b;
+let div = a / b;
+let mod = a % b;
+
+console.log (add);
+console.log (sub);
+console.log (mult);
+console.log (div);
+console.log (mod);
+
+/* Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados. */
+
