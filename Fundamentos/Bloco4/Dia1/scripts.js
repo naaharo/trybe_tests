59f49877026b6893342c40e092b1bf5b684e816f
Natã A.Rodrigues <nata26ar@gmail.com>
2021-11-26 18:37:53 -0300

    Exercicio 2 feito

--- Fundamentos/Bloco4/Dia1/scripts.js
+++ Fundamentos/Bloco4/Dia1/scripts.js
@@ -1,4 +1,5 @@
 /* Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. */
+
 const a = 5;
 const b = 3;
 
@@ -15,4 +16,17 @@ console.log (div);
 console.log (mod);
 
 /* Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados. */
+
+const num1 = 5;
+const num2 = 3;
+
+if (num1>num2){
+  console.log ("O primeiro número é maior.")
+}else if (num2>num1){
+  console.log ("O segundo número é maior.")
+}else{
+  console.log ("Os números são iguais.")
+}
+
+/* Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados. */
 
