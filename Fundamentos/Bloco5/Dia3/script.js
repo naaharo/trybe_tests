7f457f3b2e54151d78e7cb2e2c90e7f9178d68ba
Natã A.Rodrigues <nata26ar@gmail.com>
2021-12-08 17:58:16 -0300

    Dia 5.4

---
+++ Fundamentos/Bloco5/Dia3/script.js
@@ -0,0 +1,12 @@
+// HEADER
+document.getElementById('headTitle').innerText = 'Exercíco do Dia 5.3';
+document.getElementById('headSubTitle').innerText = 'JavaScript - Web Storage';
+
+// MAIN
+document.getElementById('mainTitle').innerText = 'Lorem Ipsum';
+
+function sectionsTitles()
+let classTitle = document.querySelectorAll('.title');
+for (key in classTitle) {
+  
+}
